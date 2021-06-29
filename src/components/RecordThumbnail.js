af1dba79f0030f996c7cb1b094dacea4c0dcae10
GrimmCoding <andersbiro93@gmail.com>
2021-06-28 22:07:30 -0700

    add: record thumbnail display

---
+++ src/components/RecordThumbnail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./RecordThumbnail.module.css";
+
+const RecordThumbnail = ({ basic_information }) => (
+    <article className={styles.record}>
+      <figure className={styles.record__figure}>
+        <img
+          className={styles.record__img}
+          src={basic_information.cover_image}
+          alt={basic_information.title}
+        />
+        <figcaption className={styles.record__figcaption}>
+          <h2 className={styles.record__artist}>
+            {basic_information.artists.length
+              ? basic_information.artists[0].name
+              : "Unk."}
+          </h2>
+          <h3 className={styles.record__title}>{basic_information.title}</h3>
+          <span className={styles.record__year}>
+            {basic_information.year ? basic_information.year : "Unk."}
+          </span>
+        </figcaption>
+      </figure>
+    </article>
+);
+
+export default RecordThumbnail;
