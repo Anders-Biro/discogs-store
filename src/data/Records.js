81a226fc6a0e6fbd042382b15ed9c62cd49954ca
GrimmCoding <andersbiro93@gmail.com>
2021-06-28 22:22:33 -0700

    fix: center content and reduce columns

--- src/data/Records.js
+++ src/data/Records.js
@@ -4,6 +4,6 @@ const USER = "JawKnea";
 const PUBLIC_FOLDER = "0";
 
 export const fetchReleases = (page) =>
-  makeFetch(`/users/${USER}/collection/folders/${PUBLIC_FOLDER}/releases?page=2`);
+  makeFetch(`/users/${USER}/collection/folders/${PUBLIC_FOLDER}/releases?page=${page}`);
 export const fetchRecord = id => makeFetch(`/releases/${id}`);
 export const fetchArtist = id => makeFetch(`/artists/${id}`);
