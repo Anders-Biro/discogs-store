7c8cbea1e7bc3206b8c3dce7315736e7ea4c03ed
GrimmCoding <andersbiro93@gmail.com>
2021-06-28 23:34:38 -0700

    add: pagination

--- src/Collection.js
+++ src/Collection.js
@@ -2,24 +2,78 @@ import React, { Component } from "react";
 import { fetchReleases } from "./data/Records";
 import Loader from "./components/Loader";
 import CollectionList from "./collections/CollectionList";
+import { Pagination } from "@material-ui/lab";
 
 class Collection extends Component {
-    state = { releases: [], pagination: {}, loaded: false };
+    constructor(props) {
+        super(props);
+
+        this.state = { releases: [], page: 1, count: 0, pageSize: 50, loaded: false };
+    }
+
+    pageSizes = [50, 75, 100];
 
     componentDidMount() {
-        fetchReleases().then(({ releases,  pagination }) => {
-            console.log(releases)
-            this.setState({ releases, pagination, loaded: true});
+        this.retreiveRecords(this.state.page);
+    }
+
+    retreiveRecords = () => {
+        fetchReleases(this.state.page, this.state.pageSize).then(({ releases, pagination }) => {
+            console.log(pagination)
+            this.setState({ releases: releases, page: pagination.page, count: pagination.pages, pageSize: pagination.per_page, loaded: true });
         });
     }
 
+    handlePageChange = (event, value) => {
+        this.setState(
+            {
+                releases: this.state.releases,
+                page: value,
+                count: this.state.count,
+                pageSize: event.target.value,
+                loaded: true
+            }, () => {
+                this.retreiveRecords()
+            }
+        )
+    }
+
+    handlePageSizeChange = (event) => {
+        this.setState(
+            {
+                releases: this.state.releases,
+                page: 1,
+                count: this.state.count,
+                pageSize: event.target.value,
+                loaded: true
+                
+            }, () => {
+                this.retreiveRecords();
+            }
+        )
+    }
+
     render() {
-        const { releases, pagination, loaded } = this.state;
+        const { releases, page, count, pageSize, loaded } = this.state;
 
         return (
             <div>
                 {loaded ? (
-                    <CollectionList releases={releases} />
+                    <>
+                        <CollectionList releases={releases} />
+                        <div>
+                            {"Items per Page: "}
+                            <select onChange={this.handlePageSizeChange} value={pageSize}>
+                                {this.pageSizes.map((size) => (
+                                    <option key={size} value={size}>
+                                        {size}
+                                    </option>
+                                ))}
+                            </select>
+
+                            <Pagination shape="rounded" count={count} page={page} siblingCount={1} boundaryCount={1} variant="outlined" onChange={this.handlePageChange} />
+                        </div>
+                    </>
                 ) : (
                     <Loader />
                 )}
--- src/data/Records.js
+++ src/data/Records.js
@@ -3,7 +3,7 @@ import { makeFetch } from "./utils";
 const USER = "JawKnea";
 const PUBLIC_FOLDER = "0";
 
-export const fetchReleases = (page) =>
-  makeFetch(`/users/${USER}/collection/folders/${PUBLIC_FOLDER}/releases?page=${page}`);
+export const fetchReleases = (page, perPage) =>
+  makeFetch(`/users/${USER}/collection/folders/${PUBLIC_FOLDER}/releases?page=${page}&per_page=${perPage}`);
 export const fetchRecord = id => makeFetch(`/releases/${id}`);
 export const fetchArtist = id => makeFetch(`/artists/${id}`);
